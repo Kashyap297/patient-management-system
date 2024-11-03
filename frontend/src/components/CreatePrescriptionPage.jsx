3d672380396d38fd71167a00d3968a7a741c80a5
Kashyap297 <kashyap29700@gmail.com>
2024-11-03 16:10:11 +0530

    design update in prescription

--- frontend/src/components/CreatePrescriptionPage.jsx
+++ frontend/src/components/CreatePrescriptionPage.jsx
@@ -10,14 +10,14 @@ const CreatePrescriptionPage = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[3fr_2fr] gap-4 bg-gray-100 ">
+    <div className="grid grid-cols-1 lg:grid-cols-[3fr_2fr] gap-4 bg-gray-100 min-h-full">
       {/* Left side: Prescription Form (60%) */}
-      <div className="bg-white p-6 rounded-lg shadow-lg">
+      <div className="bg-white p-6 rounded-lg shadow-lg h-full">
         <CreatePrescriptionForm onFormUpdate={handleFormUpdate} />
       </div>
 
       {/* Right side: Prescription Preview (40%) */}
-      <div className="bg-white p-6 rounded-lg shadow-lg">
+      <div className="bg-white p-6 rounded-lg shadow-lg h-full">
         <PrescriptionPreview prescriptionData={prescriptionData} />
       </div>
     </div>
